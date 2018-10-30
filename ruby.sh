8ec633e2579cfb7b132ae47db69021a9470af589
Anton Lebed <antony.lebed@gmail.com>
2018-10-30 15:20:19 +0700

    Fixed echo voice

--- ruby.sh
+++ ruby.sh
@@ -5,10 +5,10 @@ sudo apt-get update
 sudo apt-get install ruby ruby-dev make build-essential
 
 echo "export GEM_HOME=$HOME/gems" >> ~/.bashrc
-echo "path update 1 done " >&3
+echo "path update 1 done"
 
 echo "export PATH=$HOME/gems/bin:$PATH" >> ~/.bashrc
-echo "path update 2 done " >&3
+echo "path update 2 done"
 
 source ~/.bashrc
-echo "finish setup " >&3
+echo "finish setup"
