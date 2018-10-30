385531b7ff68d9a102f98470764aead11301d4b4
Anton Lebed <antony.lebed@gmail.com>
2018-10-30 14:40:49 +0700

    Server set up final and ruby draft script

---
+++ ruby.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install ruby ruby-dev make build-essential
+
+echo "export GEM_HOME=$HOME/gems" >> ~/.bashrc
+echo "export PATH=$HOME/gems/bin:$PATH" >> ~/.bashrc
+
+source ~/.bashrc
