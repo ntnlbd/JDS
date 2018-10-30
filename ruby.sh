e39ac8f0d88d1f2208801991ec8d45a6a932ec0b
Anton Lebed <antony.lebed@gmail.com>
2018-10-30 15:18:33 +0700

    Added terminal voice

--- ruby.sh
+++ ruby.sh
@@ -5,6 +5,10 @@ sudo apt-get update
 sudo apt-get install ruby ruby-dev make build-essential
 
 echo "export GEM_HOME=$HOME/gems" >> ~/.bashrc
+echo "path update 1 done " >&3
+
 echo "export PATH=$HOME/gems/bin:$PATH" >> ~/.bashrc
+echo "path update 2 done " >&3
 
 source ~/.bashrc
+echo "finish setup " >&3
